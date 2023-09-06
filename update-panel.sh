7e22d8e5793af0783928527531b0974ea4836985
RmnJL <44286466+RmnJL@users.noreply.github.com>
2023-09-06 16:34:39 +0330

    Update update-panel.sh

--- update-panel.sh
+++ update-panel.sh
@@ -1,6 +1,6 @@
 
 # Set URLs and file paths
-repoLink="https://github.com/RmnJL/HighVPN-ssh/raw/master/app.zip"
+repoLink="https://github.com/RmnJL/HighVPN-ssh/raw/main/app.zip"
 
 originalEnvFile="/var/www/html/panel/.env"
 pathDir="/var/www/html"
